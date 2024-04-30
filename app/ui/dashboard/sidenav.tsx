cf9ede401fe37e2753cb9e7b831930f0f99f6ea9
richard stuart <richardstuart007@hotmail.com>
2024-04-30 12:06:25 +1200

    library mobile

--- app/ui/dashboard/sidenav.tsx
+++ app/ui/dashboard/sidenav.tsx
@@ -8,14 +8,11 @@ import { deleteCookie } from '@/app/lib/actions'
 export default function SideNav() {
   return (
     <div className='flex h-full flex-col px-3 py-4 md:px-2'>
-      <Link
-        className='mb-2 flex h-20 items-end justify-start rounded-md bg-blue-600 p-4 md:h-40'
-        href='/'
-      >
+      <div className='mb-2 flex h-20 items-end justify-start rounded-md bg-blue-600 p-4 md:h-40'>
         <div className='w-32 text-white md:w-48'>
           <SchoolLogo />
         </div>
-      </Link>
+      </div>
       <Session />
       <div className='flex grow flex-row justify-between space-x-2 md:flex-col md:space-x-0 md:space-y-2'>
         <NavLinks />
--- app/ui/library/table.tsx
+++ app/ui/library/table.tsx
@@ -11,54 +11,19 @@ export default async function LibraryTable({
   const library = await fetchFilteredLibrary(query, currentPage)
 
   return (
-    <div className='mt-6 flow-root'>
+    <div className='mt-2 md:mt-6 flow-root'>
       <div className='inline-block min-w-full align-middle'>
         <div className='rounded-lg bg-gray-50 p-2 md:pt-0'>
-          <div className='md:hidden'>
-            {library?.map(library => (
-              <div key={library.lrlid} className='mb-2 w-full rounded-md bg-white p-4'>
-                <div className='flex items-center justify-between border-b pb-4'>
-                  <div>
-                    <div className='mb-2 flex items-center'>
-                      <p>{library.lrref}</p>
-                    </div>
-                    <p className='text-sm text-gray-500'>{library.lrdesc}</p>
-                  </div>
-                </div>
-              </div>
-            ))}
-          </div>
-          <table className='hidden min-w-full text-gray-900 md:table'>
-            <thead className='rounded-lg text-left text-sm font-normal'>
+          <table className='min-w-full text-gray-900 table-fixed'>
+            <thead className='rounded-lg text-left  font-normal text-xs md:text-sm'>
               <tr>
-                <th scope='col' className='px-4 py-5 font-medium'>
-                  Id
-                </th>
-                <th scope='col' className='px-3 py-5 font-medium'>
-                  Reference
-                </th>
-                <th scope='col' className='px-3 py-5 font-medium'>
+                <th scope='col' className='px-2 py-2 font-medium text-left'>
                   Description
                 </th>
-                <th scope='col' className='px-3 py-5 font-medium'>
-                  Who
-                </th>
-                <th scope='col' className='px-3 py-5 font-medium'>
-                  Owner
-                </th>
-                <th scope='col' className='px-3 py-5 font-medium'>
-                  Book/Video
-                </th>
-                <th scope='col' className='px-3 py-5 font-medium'>
-                  Questions
-                </th>
-                <th scope='col' className='px-3 py-5 font-medium'>
-                  Type
-                </th>
-                <th scope='col' className='px-3 py-5 font-medium'>
+                <th scope='col' className='px-2 py-2 font-medium text-centre'>
                   View
                 </th>
-                <th scope='col' className='px-3 py-5 font-medium'>
+                <th scope='col' className='px-2 py-2 font-medium text-centre'>
                   Quiz
                 </th>
               </tr>
@@ -67,28 +32,20 @@ export default async function LibraryTable({
               {library?.map(library => (
                 <tr
                   key={library.lrlid}
-                  className='w-full border-b py-3 text-sm last-of-type:border-none [&:first-child>td:first-child]:rounded-tl-lg [&:first-child>td:last-child]:rounded-tr-lg [&:last-child>td:first-child]:rounded-bl-lg [&:last-child>td:last-child]:rounded-br-lg'
+                  className='w-full border-b py-2 text-sm last-of-type:border-none [&:first-child>td:first-child]:rounded-tl-lg [&:first-child>td:last-child]:rounded-tr-lg [&:last-child>td:first-child]:rounded-bl-lg [&:last-child>td:last-child]:rounded-br-lg'
                 >
-                  <td className='whitespace-nowrap px-3 py-3'>{library.lrlid}</td>
-                  <td className='whitespace-nowrap px-3 py-3'>{library.lrref}</td>
-                  <td className='whitespace-nowrap px-3 py-3'>{library.lrdesc}</td>
-                  <td className='whitespace-nowrap px-3 py-3'>{library.lrwho}</td>
-                  <td className='whitespace-nowrap px-3 py-3'>{library.lrowner}</td>
-                  <td className='whitespace-nowrap px-3 py-3'>{library.ogcntlibrary}</td>
-                  <td className='whitespace-nowrap px-3 py-3'>
-                    {library.ogcntquestions > 0 ? library.ogcntquestions : null}
-                  </td>
-                  <td className='whitespace-nowrap px-3 py-3'>{library.lrtype}</td>
-                  <td className='whitespace-nowrap px-3 h-5'>
+                  <td className='px-2 py-1 text-xs md:text-sm'>{library.lrdesc}</td>
+                  <td className='px-2 h-5 text-centre'>
                     <BookView lrtype={library.lrtype} lrlink={library.lrlink} />
                   </td>
-                  <td className='whitespace-nowrap px-3'>
+                  <td className='px-2 text-centre'>
                     {library.ogcntquestions > 0 ? <BookQuiz lrgid={library.lrgid} /> : null}
                   </td>
                 </tr>
               ))}
             </tbody>
           </table>
+          {/*--------------------------------------------------------------*/}
         </div>
       </div>
     </div>
