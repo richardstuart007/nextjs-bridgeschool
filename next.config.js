20ef783b42e6b98363d2c85a4b37c275988768b5
richard stuart <richardstuart007@hotmail.com>
2024-11-01 22:45:45 +1300

    redirect to new website

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: '/:path*',  // Redirect all paths
+        destination: 'https://nextjs14-bridgeschool.vercel.app/:path*',  // Redirect to corresponding paths on the new site
+        permanent: true,
+      },
+    ];
+  },
+};
 
 module.exports = nextConfig;
